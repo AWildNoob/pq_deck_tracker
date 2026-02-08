6d64681ffe5a98bf9feffdde02298d4974d9408f
root <root>
2026-02-08 00:17:34 -0500

    Move base dir definition to .env

--- vite.config.ts
+++ vite.config.ts
@@ -4,5 +4,5 @@ import preact from '@preact/preset-vite'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [preact()],
-  base: (process.env.NODE_ENV === "production" ? "/pq_deck_tracker" : "/")
+  base: (process.env.NODE_ENV === "production" ? process.env["BASE_URL"] : "/")
 })
